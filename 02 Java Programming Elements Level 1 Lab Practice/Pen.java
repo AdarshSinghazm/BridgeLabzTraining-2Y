89eec08ab6fa8dd9c29b3d52710633508a25a867
AdarshSinghazm <adarshsinghsinghpurr@gmail.com>
2025-08-17 22:00:30 +0530

    Added 02 and 03 Java Programming Elements Level 2 Lab Practice folders with Java files

---
+++ 02 Java Programming Elements Level 1 Lab Practice/Pen.java
@@ -0,0 +1,16 @@
+public class Pen {
+    public static void main(String[] args) {
+
+        int pen = 14;
+        int students = 3;
+
+        int pen_division = pen / students;
+
+        int remaining_pen = pen % students;
+
+        System.out.println("The pen per student is "+pen_division+" and the remaining pen not distributed is "+remaining_pen);
+
+
+    }
+}
+
